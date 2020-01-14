b58806d1b34fcaab5c12a0fc57aedf53fb5ee3ea
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-14 03:24:43 +0200

    Adds createGraph()

--- src/com/danielgiljam/ia_2_009_0_pathfinder/Main.java
+++ src/com/danielgiljam/ia_2_009_0_pathfinder/Main.java
@@ -1,8 +1,50 @@
 package com.danielgiljam.ia_2_009_0_pathfinder;
 
+import java.util.Vector;
+
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+    }
+
+    private static Vector<Node> createGraph() {
+
+        Node hki = new Node("Helsingfors", 60.1640504, 24.7600896);
+        Node tpe = new Node("Tammerfors", 61.6277369, 23.5501169);
+        Node tku = new Node("Abo", 60.4327477, 22.0853171);
+        Node jyv = new Node("Jyväskylä", 62.1373432, 25.0954598);
+        Node kpo = new Node("Kuopio", 62.9950487, 26.556762);
+        Node lhi = new Node("Lahtis", 60.9948736, 25.5747703);
+
+        hki.addNeighbour(tpe); // Tammerfors
+        hki.addNeighbour(tku); // Åbo
+        hki.addNeighbour(lhi); // Lahtis
+
+        tpe.addNeighbour(hki); // Helsingfors
+        tpe.addNeighbour(tku); // Åbo
+        tpe.addNeighbour(jyv); // Jyväskylä
+        tpe.addNeighbour(lhi); // Lahtis
+
+        tku.addNeighbour(hki); // Helsingfors
+        tku.addNeighbour(tpe); // Tammerfors
+
+        jyv.addNeighbour(tpe); // Tammerfors
+
+        kpo.addNeighbour(lhi); // Lahtis
+
+        lhi.addNeighbour(hki); // Helsingfors
+        lhi.addNeighbour(tpe); // Tammerfors
+        lhi.addNeighbour(kpo); // Kuopio
+
+        Vector<Node> graph = new Vector<>();
+        graph.add(hki);
+        graph.add(tpe);
+        graph.add(tku);
+        graph.add(jyv);
+        graph.add(kpo);
+        graph.add(lhi);
+
+        return graph;
     }
 }
---
+++ src/com/danielgiljam/ia_2_009_0_pathfinder/Node.java
@@ -0,0 +1,22 @@
+package com.danielgiljam.ia_2_009_0_pathfinder;
+
+import java.util.Vector;
+
+public class Node {
+
+    private final String name;
+    private final double latitude;
+    private final double longitude;
+    private final Vector<Node> neighbours;
+
+    public Node(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.neighbours = new Vector<>();
+    }
+
+    public void addNeighbour(Node neighbour) {
+        neighbours.add(neighbour);
+    }
+}
