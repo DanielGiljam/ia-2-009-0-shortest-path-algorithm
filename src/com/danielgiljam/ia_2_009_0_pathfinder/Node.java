4b9a238b31ac698ed2a594136c38962192b3b354
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-15 12:04:03 +0200

    Adds dedicated types for the properties of Node
    - Adds showNodesAndLinks().

--- src/com/danielgiljam/ia_2_009_0_pathfinder/Node.java
+++ src/com/danielgiljam/ia_2_009_0_pathfinder/Node.java
@@ -1,22 +1,96 @@
 package com.danielgiljam.ia_2_009_0_pathfinder;
 
-import java.util.Vector;
+import java.util.*;
 
-public class Node {
+class Node {
 
     private final String name;
-    private final double latitude;
-    private final double longitude;
-    private final Vector<Node> neighbours;
+    private final GeoCoordinate location;
+    private final Neighbours neighbours = new Neighbours();
+    private final RoutingTable routingTable = new RoutingTable();
 
-    public Node(String name, double latitude, double longitude) {
+    Node(final String name, final double latitude, final double longitude) {
         this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.neighbours = new Vector<>();
+        location = new GeoCoordinate(latitude, longitude);
     }
 
-    public void addNeighbour(Node neighbour) {
-        neighbours.add(neighbour);
+    String getName() {
+        return name;
+    }
+
+    String[] getNeighbourNames() {
+        return neighbours.keySet().toArray(String[]::new);
+    }
+
+    void addNeighbour(final Node node) {
+        neighbours.put(node);
+    }
+
+    private static class NodeWrapper {
+        protected final String name;
+        protected final Node node;
+        protected final double distance;
+        NodeWrapper(final Node node, final double distance) {
+            this.name = node.name;
+            this.node = node;
+            this.distance = distance;
+        }
+    }
+
+    private class Neighbours extends HashMap<String, Neighbours.Neighbour> {
+
+        Neighbours(final Neighbours neighbours) {
+            super(neighbours);
+        }
+
+        Neighbours() {
+            super();
+        }
+
+        void put(final Node node) {
+            super.put(node.name, new Neighbour(node));
+        }
+
+        private class Neighbour extends NodeWrapper {
+
+            Neighbour(Node node) {
+                super(node, GeoCoordinate.getDistance(location, node.location));
+                routingTable.put(node, node, this.distance);
+            }
+        }
+    }
+
+    private static class RoutingTable extends HashMap<String, RoutingTable.Route> {
+
+        private RoutingTable(final RoutingTable routingTable) {
+            super(routingTable);
+        }
+
+        private RoutingTable() {
+            super();
+        }
+
+        private void put(final Node nextHop, final Node destination, final double distance) {
+            super.put(destination.name, new Route(nextHop, destination, distance));
+        }
+
+        private static class Route extends NodeWrapper {
+
+            private final List<Node> hops = new ArrayList<>();
+
+            private Route(final Node destination, final Node nextHop, final double distance) {
+                super(destination, distance);
+                hops.add(nextHop);
+            }
+
+            private Route(final Node destination, final List<Node> hops, final double distance) {
+                super(destination, distance);
+                this.hops.addAll(hops);
+            }
+
+            Node nextHop() {
+                return !hops.isEmpty() ? hops.get(hops.size() - 1) : null;
+            }
+        }
     }
 }
